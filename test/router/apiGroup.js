70d54e732ebae51262a96199043167b832a334a6
Josephliao1991 <gn02239440@gmail.com>
2015-09-08 18:07:47 +0800

    no message

--- test/router/apiGroup.js
+++ test/router/apiGroup.js
@@ -5,8 +5,8 @@ var group = require('../lib/group.js');
 
 //Connect TO MONGODB
 var be_ip   = "10.240.72.88:80"
-var dbName  = "myDatabase"
-mongoose.connect('mongodb://'+be_ip+"myDatabase")
+var dbName  = "/myDatabase"
+mongoose.connect('mongodb://'+be_ip+dbName)
 
 router.get('/find',function (req, res) {
   // body...
