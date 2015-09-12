9460cfd498d5af636b9ca8f6824ff202cecc83bb
Josephliao1991 <gn02239440@gmail.com>
2015-09-12 13:25:18 +0800

    no message

---
+++ APITEST/server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app =express();
+
+mongoose.connect('mongodb://10.240.42.213:27017/myDatabase')
+
+var Schema = mongoose.Schema();
+var phone  = new Schema({
+
+  name    : String,
+  number  : String
+
+})
+
+mongoose.model( 'phone', phone );
+
+
+app.get('/phoneCreate',function (req, res) {
+  // body...
+  phone.create({
+    name    : "Shit Man",
+    number  : "0800-092-000"
+  },function (error, phone) {
+    // body...
+    if (error) {
+      console.log("ERROR: "+error);
+      return res.send(error)
+    }
+
+    res.send("PHONE: "+phone)
+    // res.json(phone)
+
+
+  })
+
+})
