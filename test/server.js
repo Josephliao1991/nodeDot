e564a0cf2312b0318b7a0319da86fdb5a77feec2
Josephliao1991 <gn02239440@gmail.com>
2015-09-16 17:22:21 +0800

    no message

--- test/server.js
+++ test/server.js
@@ -18,7 +18,7 @@ var dbref = require('./dbref.js');
 app.get('/dbref',function (req, res) {
   // body...
   // dbref.dbrefTest();
-  dbref.showDbrefTest(function (error, esult) {
+  dbref.showDbrefTest(function (error, result) {
     // body...
 
     res.json(result)
