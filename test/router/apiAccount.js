64eb131974b40e59a52646e2c50bb514b8cef098
Josephliao1991 <gn02239440@gmail.com>
2015-09-17 17:39:56 +0800

    no message

--- test/router/apiAccount.js
+++ test/router/apiAccount.js
@@ -4,6 +4,32 @@ var router  = express.Router();
 var mongoose = require('mongoose');
 var person = require('../lib/person.js');
 
+
+router.get('/findAll',function (req, res) {
+  // body...
+  person.findAll(function (error,people) {
+    // body...
+    res.json(people)
+  })
+})
+
+router.get('/findById',function (req, res) {
+  // body...
+  var _id_find = req.query._id
+
+  if (!_id_find) {
+    return res.json({result : false,
+                     message : "fail,lost some params"})
+  }
+
+  person.findById(_id_find,function (error,person) {
+    // body...
+    res.json(person)
+  })
+})
+
+
+
 router.post('/regist',function (req, res) {
   // body...
   var ssid_regist = req.body.ssid
