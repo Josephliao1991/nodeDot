65fbb6d1999446e3362ef7b26f416404cbea14a5
Josephliao1991 <gn02239440@gmail.com>
2015-09-24 15:02:54 +0800

    Revert "Revert "no message""
    This reverts commit f83dc3db36bfcdd15b8172b20514927426beab0a.

--- test/router/apiDevice.js
+++ test/router/apiDevice.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var apiiNeDot = require('./apiiNeDot.js');
-
+var apiCenter = require('./apiCenter.js');
 
 router.use('/inedot',apiiNeDot)
-
+router.use('/center',apiCenter)
 
 
 module.exports = router
