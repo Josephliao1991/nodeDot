f83dc3db36bfcdd15b8172b20514927426beab0a
Josephliao1991 <gn02239440@gmail.com>
2015-09-24 15:00:58 +0800

    Revert "no message"
    This reverts commit ffc7ee241aa3eb8d529d5e25761f764feb08952b.

--- test/lib/inedot.js
+++ test/lib/inedot.js
@@ -51,7 +51,7 @@ function findByMacAddr(macAddr, callback) {
     if (error) {
       return callback(error)
     }
-    callback(null,inedot)
+    callback(null,null)
   })
 
 }
@@ -63,7 +63,7 @@ function findById(_id, callback) {
     if (error) {
       return callback(error)
     }
-    callback(null,inedot)
+    callback(null,null)
   })
 
 }
